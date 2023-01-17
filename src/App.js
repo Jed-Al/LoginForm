fa403168c6e0164e3cd94c68d432c9d457822d66
Jed <prernakumari480@gmail.com>
2023-01-18 00:19:41 +0530

    created login form UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Input, Label, Button } from "reactstrap"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="loginBox">
+      <p className="header">Sign in to your account</p>
+      <Input className="input-field emailInputField" placeholder="Email address" />
+      <Input className="input-field passwordInputField" placeholder="Password" />
+      <div className="other">
+        <Label check>
+          <Input type="checkbox" />
+          <span className="checkbox-label">Remember me</span>
+        </Label>
+        <Label className="forgot">Forgot password?</Label>
+      </div>
+      <Button color="primary" className="signInButton">Sign in</Button>
     </div>
   );
 }
